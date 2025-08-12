973fa753efff04350e45b8cd589c0788054cbb31
Santiago_PR <santiagopimentelr26@gmail.com>
2025-08-12 12:09:01 -0500

    mensaje-link component

--- src/app/components/mensaje-link/mensaje-link.component.ts
+++ src/app/components/mensaje-link/mensaje-link.component.ts
@@ -1,21 +1,24 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mensaje-link',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatDialogModule, MatButtonModule],
   templateUrl: './mensaje-link.component.html',
   styleUrl: './mensaje-link.component.scss'
 })
 export class MensajeLinkComponent implements OnInit {
 
-  mensaje
-  id
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private dialog:MatDialog ) { }
+  mensaje: string = '';
+  id: string = '';
+  
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private dialog: MatDialog) { }
 
   ngOnInit(): void {
-    this.mensaje = this.data.mensaje
-    this.id = this.data.id
+    this.mensaje = this.data.mensaje;
+    this.id = this.data.id;
   }
 }
