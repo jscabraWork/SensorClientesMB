35b9d76aab386e45a42408c5d6a1ccfa2e97c73a
Santiago_PR <123275715+SantiagoPR26@users.noreply.github.com>
2025-08-14 19:13:24 -0500

    Ajuste valor de orden en compras pendientes

--- src/app/models/compras-pendientes.model.ts
+++ src/app/models/compras-pendientes.model.ts
@@ -5,7 +5,6 @@ export class ComprasPendientesDto {
   orden: Orden;
   eventoId: number;
   eventoNombre: string;
-  precio: number;
   tickets: Ticket[];
   imagen: string;
   localidad: string;
@@ -14,7 +13,6 @@ export class ComprasPendientesDto {
     this.orden = new Orden();
     this.eventoId = 0;
     this.eventoNombre = '';
-    this.precio = 0;
     this.tickets = [];
     this.imagen = '';
     this.localidad = '';
